37ed0835b5ccf3c9919ca695da9dbbe89aba0517
@higakikeita <higaki.keichan@gmail.com>
2020-02-23 19:01:29 +0900

    ルーティングの設定

--- db/migrate/20200223092522_create_tweets.rb
+++ db/migrate/20200223092522_create_tweets.rb
@@ -3,6 +3,7 @@ class CreateTweets < ActiveRecord::Migration[5.2]
     create_table :tweets do |t|
       t.string :name
       t.string :text
+      
       t.text :image
       t.timestamps
     end
