afe9b659785c64d40a7b47e13110bfc340fef2bd
@higakikeita <higaki.keichan@gmail.com>
2020-02-23 20:32:55 +0900

    投稿削除機能の追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,8 +6,11 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
   def create
-    
     Tweet.create(tweet_params)
+  end
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
   end
   private
   def tweet_params
