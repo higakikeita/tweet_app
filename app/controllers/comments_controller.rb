6b5e212102bc9cdecd00b2d273f56e913d75d963
@higakikeita <higaki.keichan@gmail.com>
2020-02-26 00:02:21 +0900

    ビューにコメント欄実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,10 @@ class CommentsController < ApplicationController
     Comment.create(comment_params)
     redirect_to "/tweets/#{comment.tweet.id}"
   end
+  def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
 private
 def comment_params
   params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
