cefec459a50491805b27297d9ec9aa159bb667ee
@higakikeita <higaki.keichan@gmail.com>
2020-02-29 00:00:56 +0900

    コントローラ修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}"
+    redirect_to tweet_comments_path
   end
-  def show
-    @comment = Comment.new
-    @comments = @tweet.comments.includes(:user)
-  end
+  # def show
+  #   @comment = Comment.new
+  #   @comments = @tweet.comments.includes(:user)
+  # end
 private
 def comment_params
   params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
