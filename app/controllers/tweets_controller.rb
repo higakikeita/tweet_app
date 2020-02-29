4eb474ece04a2fea3959297583e2a7b9850c847c
@higakikeita <higaki.keichan@gmail.com>
2020-02-29 19:25:36 +0900

    検索ビューの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show,:serch]
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
@@ -24,6 +24,9 @@ class TweetsController < ApplicationController
   def show
     @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
+  end
+  def search
+    @tweets = Tweet.search(params[:keyword])
   end
   private
   def tweet_params
