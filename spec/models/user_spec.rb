638b21afd9ec32a373c6997fb8c5ce669a420cfd
@higakikeita <higaki.keichan@gmail.com>
2020-03-02 23:59:52 +0900

    emailテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,12 @@ describe User do
       user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
      user.valid?
      expect(user.errors[:nickname]).to include("can't be blank")
+     
+    end
+    it "is invalid without an email" do
+      user = User.new(nickname: "", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
     end
   end
 end 
